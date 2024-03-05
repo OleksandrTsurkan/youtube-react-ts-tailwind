bedd5669fae274e8aeeeb1f355974d6f1d69b3cb
Oleksandr Tsurkan <sanyaczurkan2000@gmail.com>
2024-03-05 21:07:13 +0200

    start

---
+++ src/App.tsx
@@ -0,0 +1,15 @@
+// import { useState } from "react"
+// import { CategoryPills } from "./components/CategoryPills"
+// import { categories, videos } from "./data/home"
+import { PageHeader } from "./layouts/PageHeader"
+// import { VideoGridItem } from "./components/VideoGridItem"
+// import { Sidebar } from "./layouts/Sidebar"
+// import { SidebarProvider } from "./contexts/SidebarContext"
+
+export default function App() {
+  return <div className="max-h-screen flex flex-col" >
+    <PageHeader/>
+     <div>1</div>
+ </div>
+  
+}
