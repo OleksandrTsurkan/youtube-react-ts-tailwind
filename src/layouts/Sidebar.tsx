ea7119c6a003c80ad1f851c123221515159895cd
Oleksandr Tsurkan <sanyaczurkan2000@gmail.com>
2024-03-09 21:12:23 +0200

    Sidebar finish

--- src/layouts/Sidebar.tsx
+++ src/layouts/Sidebar.tsx
@@ -29,6 +29,8 @@ import { useSidebarContext } from "../contexts/SidebarContext";
 import { PageHeaderFirstSection } from "./PageHeader";
 
 export function Sidebar() {
+  const { isLargeOpen, isSmallOpen, close } = useSidebarContext();
+
   return (
     <>
       <aside
@@ -177,6 +179,78 @@ function SmallSidebarItem({ Icon, title, url }: SmallSidebarItemProps) {
     >
       <Icon className="w-6 h-6" />
       <div className="text-sm">{title}</div>
+    </a>
+  );
+}
+
+type LargeSidebarSectionProps = {
+  children: ReactNode;
+  title?: string;
+  visibleItemCount?: number;
+};
+
+function LargeSidebarSection({
+  children,
+  title,
+  visibleItemCount = Number.POSITIVE_INFINITY,
+}: LargeSidebarSectionProps) {
+  const [isExpanded, setIsExpanded] = useState(false);
+  const childrenArray = Children.toArray(children).flat();
+  const showExpandButton = childrenArray.length > visibleItemCount;
+  const visibleChildren = isExpanded
+    ? childrenArray
+    : childrenArray.slice(0, visibleItemCount);
+  const ButtonIcon = isExpanded ? ChevronUp : ChevronDown;
+
+  return (
+    <div>
+      {title && <div className="ml-4 mt-2 text-lg mb-1">{title}</div>}
+      {visibleChildren}
+      {showExpandButton && (
+        <Button
+          onClick={() => setIsExpanded((e) => !e)}
+          variant="ghost"
+          className="w-full flex items-center rounded-lg gap-4 p-3"
+        >
+          <ButtonIcon className="w-6 h-6" />
+          <div>{isExpanded ? "Show Less" : "Show More"}</div>
+        </Button>
+      )}
+    </div>
+  );
+}
+
+type LargeSidebarItemProps = {
+  IconOrImgUrl: ElementType | string;
+  title: string;
+  url: string;
+  isActive?: boolean;
+};
+
+function LargeSidebarItem({
+  IconOrImgUrl,
+  title,
+  url,
+  isActive = false,
+}: LargeSidebarItemProps) {
+  return (
+    <a
+      href={url}
+      className={twMerge(
+        buttonStyles({ variant: "ghost" }),
+        `w-full flex items-center rounded-lg gap-4 p-3 ${
+          isActive ? "font-bold bg-neutral-100 hover:bg-secondary" : undefined
+        }`
+      )}
+    >
+      {typeof IconOrImgUrl === "string" ? (
+        <img src={IconOrImgUrl} className="w-6 h-6 rounded-full" />
+      ) : (
+        <IconOrImgUrl className="w-6 h-6" />
+      )}
+      <div className="whitespace-nowrap overflow-hidden text-ellipsis">
+        {title}
+      </div>
     </a>
   );
 }
